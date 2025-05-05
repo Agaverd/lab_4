41c56c0fa290d809d8db3bbd4d2e836087060b06
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-05 14:54:12 +0300

    add: getters for size, capaciy

--- task_3/MyArray.hpp
+++ task_3/MyArray.hpp
@@ -8,14 +8,17 @@ class ArrayT {
 public:
     ArrayT() = default;
 
+    ~ArrayT() = default;
+
     ArrayT(const ArrayT&);
 
     ArrayT(const std::ptrdiff_t size);
 
-    ~ArrayT() = default;
+    [[nodiscard]] std::ptrdiff_t Size() const noexcept {return size_;} 
+    [[nodiscard]] std::ptrdiff_t Capacity() const noexcept { return capacity_; }
 
 private:
     std::ptrdiff_t capacity_ = 0;
     std::ptrdiff_t size_ = 0;
-    std::unique_ptr<T[]> data_;
+    std::unique_ptr<T[]> data
 };

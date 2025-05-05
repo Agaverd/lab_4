4d06abf31cb9dcc30a0d1acadf80a3c8363151d3
Agaverd <agaverd.seyfaev@gmail.com>
2025-05-05 15:36:40 +0300

    add: TestSizeConstructor

--- task_3/test_3.cpp
+++ task_3/test_3.cpp
@@ -12,12 +12,27 @@ void TestDefaultConstructor() {
 	std::cout << "TestDefaultConstructor passed" << std::endl;
 }
 
+void TestSizeConstructor() {
+	ArrayT<int> arr(5);
+	assert(arr.Size() == 5);
+	assert(arr.Capacity() == 5);
+	assert(arr.IsEmpty() == false);
+
+	try {
+		ArrayT<int> invalid(-1);
+		assert(false && "Expected exception weren't thrown"); //mghtmt smart case
+	}
+	catch (const std::invalid_argument&) {
+		std::cout << "TestSizeConstructor passed" << std::endl;
+	}
+}
+
 int main() {
 	//Test cases
 	TestDefaultConstructor();
+	TestSizeConstructor();
 
 	// indev
-	//TestSizeConstructor();
 	//TestCopyConstructor();
 	//TestAssignmentOperator();
 	//TestAccessOperator();
